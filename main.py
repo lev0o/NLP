07e7196e112a7af0aafbcfe1f399e0ddc14fb512
lev0o <abdullahmohdm02@gmail.com>
2022-08-22 14:51:43 +0400

    Updating repo

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data/Tweets.csv', usecols=['text', 'selected_text', 'sentiment'])
-pd.set_option('display.max_columns', None)
-
-for col in df.columns:
-    df[col] = df[col].str.lower().str.strip().str.split(' ')
-
