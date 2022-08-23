7c6e1606b4f2b84e91761de44ec45a8f4cba79fb
lev0o <abdullahmohdm02@gmail.com>
2022-08-23 15:01:07 +0400

    Updating repo

--- predict-test.py
+++ predict-test.py
@@ -2,5 +2,9 @@ import requests
 
 url = 'http://localhost:9696/predict'
 
-response = requests.post(url, data="")
+print()
+data = input("Enter your text: ")
+print()
+
+response = requests.post(url, data=data)
 print(response._content.decode("utf-8"))
